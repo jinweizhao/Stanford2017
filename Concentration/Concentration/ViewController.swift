02f9d7a5471de57c548bae85793026337d1d49a4
jinweizhao <694951521@qq.com>
2017-12-20 15:54:16 +0800

    Concentration project initial

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by KDB on 2017/12/20.
+//  Copyright © 2017年 Will-Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
